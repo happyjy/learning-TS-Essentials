55faaa0390c9ba874c4b8d4e87de5a1519ead189
happyjy <okwoyjy@gmail.com>
2021-06-12 22:22:59 +0900

    1차 선형 학습 완료

--- 04.generic/7.Keyof&typelookup_system.js
+++ 04.generic/7.Keyof&typelookup_system.js
@@ -1,7 +1,12 @@
 "use strict";
 /*
-  # keyof & type looup system
-  * keyof 키워드, generics을 통해서 type을 찾아, 활용하는 시스템을 알아 본다.
+  # keyof & type lookup system
+    * keyof 키워드, generics을 통해서 type을 찾아 활용하는 방법을 알아본다.
+    * 확인순서 4단계
+      - [확인순서1] 문제
+      - [확인순서2] [point] keyof 란?
+      - [확인순서3] keyof를 사용해도 name을 입력했을때 return type이 string이라는 것을 보장하지 못한다.
+      - [확인순서4] keyof & generic 사용으로 호출한 프로퍼티 명의 type을 찾아 활용
 */
 var generic7 = {
     name: "jyoon",
@@ -22,7 +27,7 @@ function getProp_step1(obj, key) {
   STEP2
 */
 /*
-  * keyof를 사용해도 name을 입력했을때 return type이 string이라는 것을 보장하지 못한다.
+  * [확인순서3] keyof를 사용해도 name을 입력했을때 return type이 string이라는 것을 보장하지 못한다.
   * [point] Igeneric7[keyof Igeneric7] 의미
     => Igeneric7["name" | "age"]
     => Igeneric7["name"] | Igeneric7["age"]
@@ -33,9 +38,11 @@ function getProp_step2(obj, key) {
 }
 /*
   STEP3
+
+    * [확인순서4] keyof & generic 사용으로 호출한 프로퍼티 명의 type을 찾아 활용
     * extends: generics에서는 뒤에 나오는 타입중 하나!
     * keyof: 키워드를 사용한 객체의 타입
-    
+  
     * [point]다음 코드 분석
       ```
         getProps_step3(generic7, "name")
@@ -52,9 +59,14 @@ function getProp_step3(obj, key) {
 getProp_step3(generic7, "name");
 // function getProp_step3<Igeneric7, "age">(obj: Igeneric7, key: "age"): number
 getProp_step3(generic7, "age");
-// function setProp(obj: Igeneric7, key: "name" | "age", value: /* [문제] */string | number): void{
-//   obj[key] = value;
+// function setProp_step1(obj: Igeneric7, key: "name" | "age", value: /* [문제] */string | number): void{
+//   obj[key] = value; //error: Type 'string | number' is not assignable to type 'never'.
 // }
-function setProp(obj, key, value) {
+function setProp_step2(obj, key, value) {
     obj[key] = value;
 }
+/*
+  # generic type 추론 결과
+    * function setProp_step2<Igeneric7, "age">(obj: Igeneric7, key: "age", value: number): void
+ */
+setProp_step2(generic7, "age", 22);
